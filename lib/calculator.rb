7ee45239c9cbe3d5e08cdd8a5c7b1d5a21270c50
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-04 11:44:44 +0100

    test(feat): return sum from evaluate method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,2 +1,6 @@
 class Calculator
+
+  def evaluate(sum)
+    return sum
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,4 +1,10 @@
 require 'calculator'
 
 describe Calculator do
+
+  describe '.evaluate' do
+    it "takes in a sum and returns it to the user" do
+      expect(subject.evaluate("1 + 1")).to eq "1 + 1"
+    end
+  end
 end
