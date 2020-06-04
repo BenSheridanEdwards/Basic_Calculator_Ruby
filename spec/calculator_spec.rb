60354ca131b3bbb28bc4b0e5592fb948d5f3eb07
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-04 12:00:19 +0100

    test: larger numbers for addition

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,6 +7,10 @@ describe Calculator do
       it "takes in a sum of 1 + 1 and returns the sum with the answer of 2" do
         expect(subject.evaluate("1 + 1")).to eq ["1 + 1", 2]
       end
+
+      it "takes in a sum of 10 + 10 and returns the sum with the answer of 20" do
+        expect(subject.evaluate("10 + 10")).to eq ["10 + 10", 20]
+      end
     end
   end
 end
